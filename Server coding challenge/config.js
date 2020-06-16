e43d4d24a1e3e44b4d0c865e00c18a0159a25267
Alfredo08 <alfredosv08@gmail.com>
2020-06-15 21:43:43 -0500

    Update config.js

--- Server coding challenge/config.js
+++ Server coding challenge/config.js
@@ -1,3 +1,3 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/moviedex";
 exports.PORT = process.env.PORT || 8080;
-exports.TOKEN = process.env.TOKEN || 'exam-token';
+exports.TOKEN = process.env.TOKEN || 'success-token';
